017809b97a569148f240bbb13366465bf8a72342
onixon2010@gmail.com <onixon2010@gmail.com>
2024-03-15 08:01:27 +0800

    modified:   overlay.py

--- overlay.py
+++ overlay.py
@@ -1,7 +1,7 @@
 #To keep the windows on top of every other you can use 'Always on Top' function within 'Microsoft PowerToys' available in microsoft store for free.
 
 #---In-game settings:
-#   The script is made with respect to the following game settings. It won't work correctly if anything has been setted different.
+#   The script was tested with following game setting:
 #       Options -> Main -> Main Parameters -> UI scale = 'Large(100%)'
 #       Options -> Graphics -> Resolution = '1920 x 1080'
 #       Options -> Main -> Ground Vehicle Battle Settings -> Tactical map Scale = '133%'
@@ -9,7 +9,7 @@
 #       Options -> Main -> Battle Interface -> Safe area in HUD = '100%'
 #       Options -> Main -> Battle Interface -> HUD = 'Default'
 #---Script settings:
-map_scale = 200
+scale_map = 200
 
 #DO NOT TOUCH ANYTHING BELOW
 import cv2
@@ -19,6 +19,11 @@ import win32con
 point1 = None
 point2 = None
 selected = False
+calibrate = 1 
+scale_unit = 1
+scale_coef = 1
+distance_unit = 1
+distance_map = 1
 
 # Mouse callback function
 def select_point(event, x, y, flags, params):
@@ -32,13 +37,16 @@ def select_point(event, x, y, flags, params):
             selected = False
 
 # Create a black image, a window and bind the function to window
-image = np.zeros((432, 432, 3), dtype=np.uint8)
+image = np.zeros((520, 440, 3), dtype=np.uint8)
 cv2.namedWindow("Image")
 cv2.setMouseCallback("Image", select_point)
 
 # Calculate distance between two points
 def calculate_distance(point1, point2):
-    return np.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+    if (point1 != None) and (point2 != None):
+        return np.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+    else:
+        return False
 
 # Transparency
 def make_transparent(hwnd):
@@ -58,10 +66,15 @@ while True:
     # Legend
     if hwnd:
         make_transparent(hwnd)
-    cv2.putText(image, f"Scale (m): {map_scale:.2f}", (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),1)
-    cv2.putText(image, f"Distance (m):", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),1)
-    cv2.putText(image, f"Hold Tab to Reset", (20, 400), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),1)
-    cv2.putText(image, f"Hold ESC to exit", (20, 420), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),1)
+    if scale_coef == 1:
+        cv2.putText(image, f"Calibration:        <Calibration is needed>", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),1)
+    cv2.putText(image, f"Map scale (m): {scale_map:.2f}", (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),1)
+    cv2.putText(image, f"Calibration: {scale_coef:.2f}", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),1)
+    cv2.putText(image, f"Distance (m):", (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),1)
+    cv2.putText(image, f"Hold Tab to Reset", (20, 480), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 255, 255),1)
+    cv2.putText(image, f"Hold ESC to Exit", (20, 490), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 255, 255),1)
+    cv2.putText(image, f"Hold Tilda (~) to Calibrate", (20, 500), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 255, 255),1)
+    cv2.putText(image, f"Calibration: set 'scale_map'; measure 5 map squares with 2 dots; depress Tilda; depress Tab.", (20, 510), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 255, 255),1)
 
     # Draw
     cv2.imshow("Image", image)
@@ -69,15 +82,24 @@ while True:
         cv2.circle(image, point1, 5, (0, 255, 0), -1)
     if point2 is not None:
         cv2.circle(image, point2, 5, (0, 0, 255), -1)
-        distance = calculate_distance(point1, point2) * 3.478 * (map_scale/200)
-        cv2.putText(image, f"Distance (m): {distance:.2f}", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),1)
+        distance_unit = calculate_distance(point1, point2)
+        distance_map = distance_unit*scale_coef
+        cv2.putText(image, f"Distance (m): {distance_map:.2f}", (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),1)
     
     # Reset
-    if cv2.waitKey(1) & 0xFF == 9:     # '~' key
-        image = np.zeros((432, 432, 3), dtype=np.uint8)
-        point1 = None
+    if cv2.waitKey(1) & 0xFF == 9:     # 'Tab' key
+        image = np.zeros((520, 440, 3), dtype=np.uint8)
+        point1 = None   
         point2 = None
         selected = False
+
+    # Calibrate
+    if cv2.waitKey(1) & 0xFF == 96:     # '~' key
+        calibrate = calculate_distance(point1, point2)
+        if calibrate!=False:
+            scale_unit = calibrate/5
+            scale_map = scale_map
+            scale_coef = scale_map/scale_unit
     
     # Exit
     if cv2.waitKey(1) & 0xFF == 27:     # 'ESC' key
